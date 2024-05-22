150debbb7dc170e1a9f7d1969771e9d51a764f05
Albert Kwok <albert.kwok@oracle.com>
2024-05-22 15:16:27 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,7 +1,22 @@
 ## Copyright (c) 2021, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
 # 
-
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+    }
+    local = {
+      source  = "hashicorp/local"
+    }
+    random = {
+      source  = "hashicorp/random"
+    }
+  }
+}
 provider "oci" {
             tenancy-ocid = var.tenancy-ocid
             region       = var.region
